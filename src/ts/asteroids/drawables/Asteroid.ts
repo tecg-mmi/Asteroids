10497f089f3464428daf6920118e45ffca8dc2e2
Daniel Schreurs <daniel.schreurs@hotmail.com>
2024-04-06 16:01:43 +0200

    :sparkles: Introducing Asteroids

---
+++ src/ts/asteroids/drawables/Asteroid.ts
@@ -0,0 +1,95 @@
+import {IAnimatable} from "../../framework/types/IAnimatable";
+import {settings} from "../settings";
+import {Circle} from "../../framework/shapes/Circle";
+import {Vector} from "../../framework/Vector";
+import {Random} from "../../framework/helpers/Random";
+import {Rgb} from "../../framework/colors/Rgb";
+import {Animate} from "../../framework/Animate";
+import {Bullet} from "./Bullet";
+
+export class Asteroid extends Circle implements IAnimatable {
+    private readonly canvas: HTMLCanvasElement;
+    private readonly speed: Vector;
+    private path: Path2D;
+    shouldBeRemove: boolean;
+    private readonly animation: Animate;
+    private scale: number;
+    private parent: Asteroid;
+    private acceleration: Vector;
+
+    constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, animation: Animate, parent?: Asteroid) {
+        super(ctx, parent ? Rgb.blue : settings.asteroid.color, new Vector({
+            x: parent ? parent.position.x : Random.int(0, canvas.width),
+            y: parent ? parent.position.y : Random.int(0, canvas.height)
+        }), Random.float(0, Math.PI * 2), settings.asteroid.radius, false);
+        this.parent = parent;
+        this.canvas = canvas;
+        this.shouldBeRemove = false;
+        this.animation = animation;
+        if (parent) {
+            this.scale = parent.scale / 2;
+        } else {
+            this.scale = Random.float(settings.asteroid.scale.min, settings.asteroid.scale.max);
+        }
+        this.speed = new Vector({x: 0, y: 0});
+        this.acceleration = Vector.fromAngle(this.degree, Random.int(settings.asteroid.speed.min, settings.asteroid.speed.max));
+        this.speed.add(this.acceleration);
+        this.generatePath();
+    }
+
+
+    draw(): void {
+        this.ctx.save();
+        this.ctx.translate(this.position.x, this.position.y);
+        //this.ctx.rotate(this.degree);
+        this.ctx.strokeStyle = this.color.toString();
+        this.ctx.stroke(this.path);
+        this.ctx.restore();
+    }
+
+    checkEdges() {
+        if (this.position.y > this.canvas.height + settings.asteroid.maxSize) {
+            this.position.y = -settings.asteroid.maxSize;
+        }
+        if (this.position.y < -settings.asteroid.maxSize) {
+            this.position.y = this.canvas.height + settings.asteroid.maxSize;
+        }
+        if (this.position.x > this.canvas.width + settings.asteroid.maxSize) {
+            this.position.x = -settings.asteroid.maxSize;
+        }
+        if (this.position.x < -settings.asteroid.maxSize) {
+            this.position.x = this.canvas.width + settings.asteroid.maxSize;
+        }
+    }
+
+    update(): void {
+        (this.position as Vector).add(this.speed);
+        this.checkEdges();
+        this.checkCollision();
+    }
+
+    private generatePath() {
+        if (this.parent) {
+            this.path = new Path2D(settings.asteroid.smallShapes[Random.int(0, settings.asteroid.shapes.length)]);
+        } else {
+            this.path = new Path2D(settings.asteroid.shapes[Random.int(0, settings.asteroid.shapes.length)]);
+        }
+        this.path.closePath();
+    }
+
+    private checkCollision() {
+        this.animation.iAnimates.forEach((bullet: IAnimatable) => {
+            if (bullet instanceof Bullet) {
+                if (this.ctx.isPointInPath(this.path, bullet.position.x - this.position.x, bullet.position.y - this.position.y)) {
+                    this.color = Rgb.red;
+                    if (this.scale > settings.asteroid.scale.min / 2) {
+                        for (let i = 0; i < settings.asteroid.newAsteroidsCount; i++) {
+                            this.animation.registerForAnimation(new Asteroid(this.ctx, this.canvas, this.animation, this));
+                        }
+                    }
+                    this.shouldBeRemove = true;
+                }
+            }
+        });
+    }
+}
