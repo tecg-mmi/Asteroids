323f8b80e29688c63f4698972861c9a9e4e0a3b9
Daniel Schreurs <daniel.schreurs@hotmail.com>
2024-04-06 17:49:17 +0200

    :sparkles: Introducing point for triangle for beter collision detection.

--- src/ts/framework/shapes/Triangle.ts
+++ src/ts/framework/shapes/Triangle.ts
@@ -5,11 +5,17 @@ import {IPosition} from "../types/iPosition";
 export class Triangle extends Shape {
     protected readonly width: number;
     protected readonly height: number;
+    public points: IPosition[];
 
-    constructor(ctx: CanvasRenderingContext2D, position:IPosition, width: number, height: number, color: IColor, degree: number = 0, isFilled: boolean = true) {
-        super(ctx, color, position,degree, isFilled);
+    constructor(ctx: CanvasRenderingContext2D, position: IPosition, width: number, height: number, color: IColor, degree: number = 0, isFilled: boolean = true) {
+        super(ctx, color, position, degree, isFilled);
         this.width = Math.trunc(width)
         this.height = Math.trunc(height);
+        this.points = [
+            {x: 0, y: -this.height / 2},
+            {x: -this.width / 2, y: this.height / 2},
+            {x: this.width / 2, y: this.height / 2}
+        ];
     }
 
     public draw() {
@@ -17,9 +23,9 @@ export class Triangle extends Shape {
         this.ctx.translate(this.position.x, this.position.y);
         this.ctx.rotate(this.degree);
         this.ctx.beginPath();
-        this.ctx.moveTo(0, -this.height / 2);
-        this.ctx.lineTo(-this.width / 2, this.height / 2);
-        this.ctx.lineTo(this.width / 2, this.height / 2);
+        this.ctx.moveTo(this.points[0].x, this.points[0].y);
+        this.ctx.lineTo(this.points[1].x, this.points[1].y);
+        this.ctx.lineTo(this.points[2].x, this.points[2].y);
         this.ctx.closePath();
         this.fillOrStroke();
         this.ctx.restore();
