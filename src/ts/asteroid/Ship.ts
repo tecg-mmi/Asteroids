d5edf1d2ff9600d382322bd5cc1e8fe2200ca591
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-04-22 18:32:40 +0200

    Check edges for ship

--- src/ts/asteroid/Ship.ts
+++ src/ts/asteroid/Ship.ts
@@ -23,10 +23,24 @@ export class Ship extends Triangle implements iAnimatable {
         this.handleKeys();
         this.speed.multiply(settings.ship.friction);
         this.position.add(this.speed);
-
+        this.checkEdges();
         this.draw();
     }
 
+    checkEdges() {
+        if (this.position.y > this.canvas.height + settings.ship.height) {
+            this.position.y = -settings.ship.height;
+        }
+        if (this.position.y < -settings.ship.height) {
+            this.position.y = this.canvas.height + settings.ship.height;
+        }
+        if (this.position.x > this.canvas.width + settings.ship.width) {
+            this.position.x = -settings.ship.width;
+        }
+        if (this.position.x < -settings.ship.width) {
+            this.position.x = this.canvas.width + settings.ship.width;
+        }
+    }
 
     private handleKeys() {
         this.keyController.currentKeys.forEach((key) => {
