17788ee0e44ec2df39fcd8b30c49d0733beb95d2
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-04-23 09:39:43 +0200

    Init asteroid

--- src/ts/asteroid/Asteroid.ts
+++ src/ts/asteroid/Asteroid.ts
@@ -1,27 +1,26 @@
-import {Shape} from "../framework25/shapes/Shape";
 import {iAnimatable} from "../framework25/types/iAnimatable";
 import {Vector} from "../framework25/Vector";
 import {settings} from "./settings";
 import {randomFloat, randomInt} from "../framework25/helpers/random";
+import {Rectangle} from "../framework25/shapes/Rectangle";
+import {Collision} from "../framework25/helpers/Collision";
 
-export class Asteroid extends Shape implements iAnimatable {
-    private canvas: HTMLCanvasElement;
-    private path: Path2D;
-    private direction: number;
-    private selfRotation: number;
-    private speed: Vector;
-    private acceleration: Vector;
-    private rotationSpeed: number;
+export class Asteroid extends Rectangle implements iAnimatable {
+    private readonly canvas: HTMLCanvasElement;
+    private readonly path: Path2D;
+    private readonly direction: number;
+    private readonly speed: Vector;
+    private readonly acceleration: Vector;
+    private readonly rotationSpeed: number;
 
     constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
         super(ctx, new Vector({
             x: randomInt(settings.asteroid.size / 2, canvas.width - settings.asteroid.size / 2),
             y: randomInt(settings.asteroid.size / 2, canvas.height - settings.asteroid.size / 2)
-        }), settings.asteroid.color);
+        }), settings.asteroid.color, settings.asteroid.size, settings.asteroid.size, 0);
         this.canvas = canvas;
         this.path = new Path2D(settings.asteroid.shapes[randomInt(0, settings.asteroid.shapes.length - 1)]);
         this.path.closePath();
-        this.selfRotation = 0;
         this.direction = randomFloat(0, Math.PI * 2);
         this.speed = new Vector({x: 0, y: 0});
         this.acceleration = Vector.fromAngle(this.direction, randomInt(settings.asteroid.acceleration.min, settings.asteroid.acceleration.max));
@@ -31,7 +30,8 @@ export class Asteroid extends Shape implements iAnimatable {
 
     update() {
         this.position.add(this.speed);
-        this.selfRotation += this.rotationSpeed;
+        this.rotation += this.rotationSpeed;
+        Collision.replaceOutOfBounds(this, this.canvas);
     }
 
     animate(): void {
@@ -40,10 +40,10 @@ export class Asteroid extends Shape implements iAnimatable {
     }
 
 
-    private draw() {
+    public draw() {
         this.ctx.save();
         this.ctx.translate(this.position.x, this.position.y);
-        this.ctx.rotate(this.selfRotation);
+        this.ctx.rotate(this.rotation);
         this.ctx.translate(-settings.asteroid.size / 2, -settings.asteroid.size / 2);
         this.ctx.strokeStyle = this.color.toString();
         this.ctx.stroke(this.path);
